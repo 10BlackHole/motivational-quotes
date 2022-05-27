d13fb15737570a1a19de31aef17a82737b27e3ab
10BlackHole <borjadiez1014@gmail.com>
2022-05-27 12:39:25 -0400

    add more quotes

--- quotes.py
+++ quotes.py
@@ -2,9 +2,11 @@ import numpy as np
 
 # quotes = ["However bad life may seem, there is always something you can do and succeed at. Where there's life, there's hope. - Stephen Hawking"]
 quotes = np.genfromtxt("quotes.txt", dtype='str', delimiter='-')
+
 # phrase = quotes[:,0]
 # author = quotes[:,1]
-print(len(quotes))
+# print(quotes)
+
 # def add():
     # print("Add a motivational quote")
     # phrase = str(input())
