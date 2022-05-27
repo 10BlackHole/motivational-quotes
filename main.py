90c80723576f85d2004554f387c615b075a3a255
10BlackHole <borjadiez1014@gmail.com>
2022-05-27 16:28:45 -0400

    ksa

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import random
+import tkinter as tk
 from quotes import quotes, phrases, authors
 
-def main():
+def text():
     n = random.randint(0, len(quotes)-1)
-    print(phrases[n] + ' - ' + authors[n])
+    # print(phrases[n] + ' - ' + authors[n])
+    return(phrases[n] + ' - ' + authors[n])
+
+def ventana(x):
+    ventana = tk.Tk()
+    ventana.title("Motivation Quote")
+    ventana.geometry("1200x300")
+    ventana.configure(bg="dark turquoise")
+    label1 = tk.Label(ventana, text=x)
+    label1.pack()
+    ventana.mainloop()
+
+def main():
+    return ventana(text())
 
 main()
-
 
---
+++ ventana.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.title("Motivation Quote")
+ventana.geometry("400x400")
+ventana.configure(bg='dark turquoise')
+
+label1 = tk.Label(ventana, text="")
+
+ventana.mainloop()
